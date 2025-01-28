af2b1bd7d0f62cc0f6b8c5d985babda3e0be5abe
sai7731 <saicharan773183@gmail.com>
2025-01-28 12:26:43 +0530

    Create Label encoding.py

---
+++ Label encoding.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
+data = {
+    "customer_id": [1, 2, 3, 4],
+    "gender": ["male", "female", "male", "female"],
+    "city": ["Hyderabad", "pune", "bangalore", "mumbai"],
+    "fruits": ["apple", "orange", "banana", "kivi"]
+}
+
+df = pd.DataFrame(data)
+print("Original DataFrame:")
+print(df)
+
+one_hot_encoder = OneHotEncoder(sparse_output=False)
+columns_to_encode = ["gender", "city", "fruits"]
+encoded_data = one_hot_encoder.fit_transform(df[columns_to_encode])
+encoded_columns = one_hot_encoder.get_feature_names_out(columns_to_encode)
+encoded_df = pd.DataFrame(encoded_data, columns=encoded_columns)
+
+print("\nOne_Hot Encoded DataFrame:")
+print(encoded_df)
